ac7c22fae6de85d637bf84ced84e8093201d2612
Vakhushti Metreveli <vakhushti.metreveli@gmail.com>
2021-05-13 23:07:23 +0400

    [Authporation]

--- CleanArchitecture.API/Controllers/ApiController.cs
+++ CleanArchitecture.API/Controllers/ApiController.cs
@@ -1,10 +1,12 @@
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 namespace API.Controllers
 {
 
     [ApiController]
+    [Authorize]
     // [Route("api/[controller]")]
     [Route("api/v{version:apiVersion}/[controller]")]
     public class ApiController : ControllerBase
