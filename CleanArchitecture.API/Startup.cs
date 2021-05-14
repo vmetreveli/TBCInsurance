e1cd9f922916cf2d7640a000b9d76434363fe7db
vakhushti-lemondo <vakhushti@lemondo.com>
2021-05-14 15:02:33 +0400

    config swagger step1

--- CleanArchitecture.API/Startup.cs
+++ CleanArchitecture.API/Startup.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Text;
-using CleanArchitecture.Application.Interfaces;
-using CleanArchitecture.Application.Services;
+using API.Swagger;
 using CleanArchitecture.Common.AppSettings;
 using CleanArchitecture.Domain.Models.Entities;
 using CleanArchitecture.Infrastructure.IoC;
@@ -16,7 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
+
 namespace API
 {
     public class Startup
@@ -34,8 +33,18 @@ namespace API
             //Configuration from AppSettings
             services.Configure<JWT>(Configuration.GetSection("JWT"));
             //User Manager Service
-            services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<UniDbContext>();
-          
+            services.AddIdentity<User, IdentityRole>(settings =>
+            {
+                settings.Lockout.MaxFailedAccessAttempts = 3;
+                settings.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(10);
+                settings.Password.RequiredLength = 4;
+                settings.Password.RequireNonAlphanumeric = false;
+                settings.Password.RequireUppercase = false;
+                settings.Password.RequireDigit = false;
+                settings.Password.RequireLowercase = false;
+                settings.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<UniDbContext>().AddDefaultTokenProviders();
+            services.AddControllersWithViews();
 
             services.AddDbContext<UniDbContext>(options =>
                 options.UseSqlite(
@@ -71,10 +80,11 @@ namespace API
 
 
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanArchitecture.API", Version = "v1" });
-            });
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "CleanArchitecture.API", Version = "v1" });
+            //     
+            // });
 
             #region Api Versioning
 
@@ -95,7 +105,7 @@ namespace API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -122,6 +132,9 @@ namespace API
             {
                 endpoints.MapControllers();
             });
+            //Enable Swagger
+            app.UseSwaggerDocumentation(Configuration.GetValue<string>("IdentityServerOptions:AuthClientId"), Configuration.GetValue<string>("IdentityServerOptions:AuthClientSecret"));
+
         }
 
         private static void RegisterServices(IServiceCollection services)
